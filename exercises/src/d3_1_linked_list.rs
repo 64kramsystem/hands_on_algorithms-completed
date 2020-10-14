1c2631956a2749871ed04ae9a3cc59d4a68acdc6
Saverio Miroddi <saverio.pub2@gmail.com>
2020-10-14 21:33:11 +0200

    Implement linked list

---
+++ exercises/src/d3_1_linked_list.rs
@@ -0,0 +1,66 @@
+// PartialOrd is for the exercise.
+// Copy is for the list values function.
+//
+struct LinkedList<T: PartialOrd + Copy>(Option<(T, Box<LinkedList<T>>)>);
+
+impl<T: PartialOrd + Copy> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
+        LinkedList(None)
+    }
+
+    pub fn push_front(&mut self, data: T) {
+        let previous = self.0.take();
+        self.0 = Some((data, Box::new(LinkedList(previous))));
+    }
+
+    pub fn push_back_iterative(&mut self, data: T) {
+        let mut current = self;
+
+        while let Some((_, ref mut child)) = current.0 {
+            current = child;
+        }
+
+        current.push_front(data);
+    }
+
+    pub fn push_back_recursive(&mut self, data: T) {
+        match self.0 {
+            Some((_, ref mut child)) => child.push_back_recursive(data),
+            None => self.push_front(data),
+        }
+    }
+
+    pub fn values(&self) -> Vec<T> {
+        let mut values = vec![];
+
+        let mut current = self;
+
+        while let Some((ref child_data, ref child)) = current.0 {
+            values.push(*child_data);
+            current = child;
+        }
+
+        values
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_back_iterative() {
+        let values = vec![
+            1273, 18273, 8273, 827, 11, 213, 9172397, 2373, 2, 4, 20983, 29831093, 287, 2837, 11,
+            92900,
+        ];
+
+        let mut list = LinkedList::new();
+
+        for value in &values {
+            list.push_back_iterative(*value);
+        }
+
+        assert_eq!(list.values()[..], values[..]);
+    }
+}
